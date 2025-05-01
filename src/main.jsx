d155920d5b107d338d43261de2d52c8edb77e890
adamdenis <Boiteasouvenir@hotmail.com>
2025-05-01 15:59:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../i18n/locales/config.js'; // Import i18n configuration
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './src/index.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
