bf49d3a66e9dc03f96495a6be2ee76465129424e
adamdenis <Boiteasouvenir@hotmail.com>
2025-05-01 15:56:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../i18n/locales/config.js'; // Import i18n configuration
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';
+import App from './App.jsx';
 import './src/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
