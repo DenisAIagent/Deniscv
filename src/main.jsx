d89d1e9468554595997ce2a96e82a3b63e62486a
adamdenis <Boiteasouvenir@hotmail.com>
2025-05-01 16:10:52 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// Chemin corrigé ici (./i18n/... car i18n est au même niveau que main.jsx dans src)
-import './i18n/locales/config.js'; // Import i18n configuration
+import '../i18n/locales/config.js'; // Import i18n configuration
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Ce chemin est correct (App.jsx est au même niveau que main.jsx dans src)
 import './index.css'; // Ce chemin est correct (index.css est au même niveau que main.jsx dans src)
